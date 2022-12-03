bd03a9fdd266607966ddc3d05670885ce825ecc5
GHIOMA <martinezgabrielalejandro4@gmail.com>
2022-12-03 14:52:14 -0300

    bottom correction

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,14 @@
- const navButtonMenu = document.querySelector(".nav__button");
- const navDiv = document.querySelector(".nav__div");
+const navButtonMenu = document.querySelector(".nav__button");
+const navDiv = document.querySelector(".nav__div");
 
- navButtonMenu.addEventListener("click", ()=>{
-    navDiv.classList.toggle("nav__button--togle")
- });
-
- const navButtonCross = document.querySelector(".nav__button--cross");
-
-
-
- navButtonCross.addEventListener("click", ()=>{
+navButtonMenu.addEventListener("click", () => {
    navDiv.classList.toggle("nav__button--togle")
 });
+
+const navButtonCross = document.querySelector(".nav__button--cross");
+
+navButtonCross.addEventListener("click", () => {
+   navDiv.classList.toggle("nav__button--togle")
+});
+
+
